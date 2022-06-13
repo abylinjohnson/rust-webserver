9b4df476a1e447b67b42c8a8686146653c024f82
Abylin Johnson <aj@pop-os.localdomain>
2022-06-14 04:24:16 +0530

    Added Thread Pool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,13 +2,18 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs;
+use web_server::ThreadPool;
+
 fn main() {
     let listener: TcpListener = 
         TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming(){
         let stream = stream.unwrap();
         println!("Connection Established");
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 
 }
